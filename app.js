f354c7be8b0b3631e52b50cae76479dcb5eb242b
sumit87814 <sumit87814@gmail.com>
2026-04-21 21:28:07 +0530

    first phase

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+const heading = React.createElement("h1", { id: "heading" }, "Hello react sumit");
+
+console.log(heading); //object
+
+const root= ReactDOM.createRoot(document.getElementById("title"));
+root.render(heading);
+    
