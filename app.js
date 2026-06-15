d32abe2fc1a044605c0a65714e0a2a8d84e7a51b
sumit87814 <sumit87814@gmail.com>
2026-06-15 13:25:54 +0530

    cost for two

--- app.js
+++ app.js
@@ -23,28 +23,287 @@ const Header=()=>{
 const styleCard = {
   backgroundColor: "grey"
 };
-const RestaurantCard=(props)=>{
+const RestaurantCard = (props) => {
+  const { resData } = props;
     return(
       <div className="res-card" style={styleCard}>
         <img className="res-logo"
-           src="https://images.unsplash.com/photo-1631515243349-e0cb75fb8d3a?q=80&w=1188&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Biryani image" />
-        <h3>{ props.resName}</h3>
-        <h3>{ props.cusine}</h3>
-        <h3>4.4 Starts</h3>
-        <h3>38 minutes</h3>
+          src={"https://media-assets.swiggy.com/swiggy/image/upload/"+ resData.info.cloudinaryImageId} alt="Biryani image" />
+        <h3>{ resData.info.name }</h3>
+        <h3>{ resData.info.cuisines.join(", ") }</h3>
+        <h3>{ resData.info.avgRating } Starts</h3>
+        <h3>{resData.info.sla.deliveryTime} minutes</h3>
+        <h3>{resData.info.costForTwo}</h3>
+        <h3>{resData.info.aggregatedDiscountInfoV3.header}</h3>
         </div>
     );
 };
+
+const resObj = {
+  "restaurants": [
+                    {
+                      "info": {
+                        "id": "123456",
+                        "name": "Pizza Paradise",
+                        "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/17/6def0f0f-9e6c-45c0-b5e6-05af750f27b5_795906.JPG",
+                        "locality": "MG Road",
+                        "areaName": "Central District",
+                        "costForTwo": "₹400 for two",
+                        "cuisines": [
+                          "Pizza",
+                          "Italian",
+                          "Fast Food"
+                        ],
+                        "avgRating": 4.3,
+                        "avgRatingString": "4.3",
+                        "totalRatingsString": "10K+ ratings",
+                        "veg": false,
+                        "sla": {
+                          "deliveryTime": 30,
+                          "lastMileTravel": 3.5,
+                          "slaString": "30 mins"
+                        },
+                        "aggregatedDiscountInfoV3": {
+                          "header": "50% OFF",
+                          "subHeader": "UPTO ₹100"
+                        }
+                      }
+                    },
+                    {
+                      "info": {
+                        "id": "234567",
+                        "name": "Burger Hub",
+                        "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/6/24/acfcaacc-edf0-4189-8264-d614d312c0ee_740457.JPG",
+                        "locality": "Park Street",
+                        "areaName": "Downtown",
+                        "costForTwo": "₹300 for two",
+                        "cuisines": [
+                          "Burgers",
+                          "American",
+                          "Fast Food"
+                        ],
+                        "avgRating": 4.5,
+                        "avgRatingString": "4.5",
+                        "totalRatingsString": "15K+ ratings",
+                        "veg": false,
+                        "sla": {
+                          "deliveryTime": 25,
+                          "lastMileTravel": 2,
+                          "slaString": "25 mins"
+                        },
+                        "aggregatedDiscountInfoV3": {
+                          "header": "40% OFF",
+                          "subHeader": "UPTO ₹80"
+                        }
+                      }
+                    },
+                    {
+                      "info": {
+                        "id": "345678",
+                        "name": "Green Bites",
+                        "cloudinaryImageId": "e0839ff574213e6f35b3899ebf1fc597",
+                        "locality": "Residency Road",
+                        "areaName": "South Zone",
+                        "costForTwo": "₹250 for two",
+                        "cuisines": [
+                          "Healthy Food",
+                          "Salads",
+                          "Vegan"
+                        ],
+                        "avgRating": 4.7,
+                        "avgRatingString": "4.7",
+                        "totalRatingsString": "8K+ ratings",
+                        "veg": true,
+                        "sla": {
+                          "deliveryTime": 20,
+                          "lastMileTravel": 1.5,
+                          "slaString": "20 mins"
+                        },
+                        "aggregatedDiscountInfoV3": {
+                          "header": "30% OFF",
+                          "subHeader": "UPTO ₹75"
+                        }
+                      }
+                    },
+                    {
+                      "info": {
+                        "id": "456789",
+                        "name": "Spice Kingdom",
+                        "cloudinaryImageId": "rng/md/carousel/production/indian101",
+                        "locality": "Brigade Road",
+                        "areaName": "City Center",
+                        "costForTwo": "₹500 for two",
+                        "cuisines": [
+                          "Indian",
+                          "North Indian",
+                          "Biryani"
+                        ],
+                        "avgRating": 4.2,
+                        "avgRatingString": "4.2",
+                        "totalRatingsString": "12K+ ratings",
+                        "veg": false,
+                        "sla": {
+                          "deliveryTime": 35,
+                          "lastMileTravel": 4,
+                          "slaString": "35 mins"
+                        },
+                        "aggregatedDiscountInfoV3": {
+                          "header": "20% OFF",
+                          "subHeader": "UPTO ₹50"
+                        }
+                      }
+                    },
+                    {
+                      "info": {
+                        "id": "567890",
+                        "name": "Chinese Dragon",
+                        "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/18/ba9f1f59-30d5-44de-afad-df6db8471ead_9648.jpg",
+                        "locality": "Commercial Street",
+                        "areaName": "East District",
+                        "costForTwo": "₹350 for two",
+                        "cuisines": [
+                          "Chinese",
+                          "Asian",
+                          "Thai"
+                        ],
+                        "avgRating": 4.4,
+                        "avgRatingString": "4.4",
+                        "totalRatingsString": "9K+ ratings",
+                        "veg": false,
+                        "sla": {
+                          "deliveryTime": 28,
+                          "lastMileTravel": 2.8,
+                          "slaString": "28 mins"
+                        },
+                        "aggregatedDiscountInfoV3": {
+                          "header": "60% OFF",
+                          "subHeader": "UPTO ₹120"
+                        }
+                      }
+                    },
+                    {
+                      "info": {
+                        "id": "678901",
+                        "name": "Dessert Delight",
+                        "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/11/8/731001f1-f1c4-4f5f-849f-79a697cb0b72_390173.jpg",
+                        "locality": "Lavelle Road",
+                        "areaName": "West End",
+                        "costForTwo": "₹200 for two",
+                        "cuisines": [
+                          "Desserts",
+                          "Ice Cream",
+                          "Bakery"
+                        ],
+                        "avgRating": 4.6,
+                        "avgRatingString": "4.6",
+                        "totalRatingsString": "7K+ ratings",
+                        "veg": true,
+                        "sla": {
+                          "deliveryTime": 22,
+                          "lastMileTravel": 1.8,
+                          "slaString": "22 mins"
+                        },
+                        "aggregatedDiscountInfoV3": {
+                          "header": "25% OFF",
+                          "subHeader": "UPTO ₹60"
+                        }
+                      }
+                    },
+                    {
+                      "info": {
+                        "id": "789012",
+                        "name": "Sushi Station",
+                        "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/6/24/acfcaacc-edf0-4189-8264-d614d312c0ee_740457.JPG",
+                        "locality": "Indiranagar",
+                        "areaName": "Uptown",
+                        "costForTwo": "₹800 for two",
+                        "cuisines": [
+                          "Japanese",
+                          "Sushi",
+                          "Asian"
+                        ],
+                        "avgRating": 4.8,
+                        "avgRatingString": "4.8",
+                        "totalRatingsString": "5K+ ratings",
+                        "veg": false,
+                        "sla": {
+                          "deliveryTime": 40,
+                          "lastMileTravel": 5.2,
+                          "slaString": "40 mins"
+                        },
+                        "aggregatedDiscountInfoV3": {
+                          "header": "15% OFF",
+                          "subHeader": "UPTO ₹150"
+                        }
+                      }
+                    },
+                    {
+                      "info": {
+                        "id": "890123",
+                        "name": "South Spice",
+                        "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/17/6def0f0f-9e6c-45c0-b5e6-05af750f27b5_795906.JPG",
+                        "locality": "Jayanagar",
+                        "areaName": "South Bangalore",
+                        "costForTwo": "₹300 for two",
+                        "cuisines": [
+                          "South Indian",
+                          "Dosa",
+                          "Idli"
+                        ],
+                        "avgRating": 4.5,
+                        "avgRatingString": "4.5",
+                        "totalRatingsString": "11K+ ratings",
+                        "veg": true,
+                        "sla": {
+                          "deliveryTime": 25,
+                          "lastMileTravel": 3,
+                          "slaString": "25 mins"
+                        },
+                        "aggregatedDiscountInfoV3": {
+                          "header": "35% OFF",
+                          "subHeader": "UPTO ₹70"
+                        }
+                      }
+                    },
+                    {
+                      "info": {
+                        "id": "901234",
+                        "name": "Pasta Palace",
+                        "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/10/17/7bd350a8-55e7-459b-83a2-e250e670d194_14558.JPG",
+                        "locality": "Koramangala",
+                        "areaName": "Tech Hub",
+                        "costForTwo": "₹450 for two",
+                        "cuisines": [
+                          "Italian",
+                          "Pasta",
+                          "Continental"
+                        ],
+                        "avgRating": 4.1,
+                        "avgRatingString": "4.1",
+                        "totalRatingsString": "6K+ ratings",
+                        "veg": false,
+                        "sla": {
+                          "deliveryTime": 32,
+                          "lastMileTravel": 3.8,
+                          "slaString": "32 mins"
+                        },
+                        "aggregatedDiscountInfoV3": {
+                          "header": "45% OFF",
+                          "subHeader": "UPTO ₹90"
+                        }
+                      }
+                    }
+                  ]
+}
 
 const Body=()=>{
     return(
       <div className="body">
         <div className="search">Search</div>
         <div className="res-container">
-          <RestaurantCard resName="Behen da dhaba" cusine="Biryani" />
-          <RestaurantCard resName="Pizza world" cusine="Pizza" />
-          <RestaurantCard resName="Kfc" cusine="Fast Food" />
-          <RestaurantCard resName="Burger King" cusine="Burgers" />
+        {resObj.restaurants.map((restaurant) => (
+            <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+          ))}
         </div>
       </div>
     );
